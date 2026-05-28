30ea48f21db8600f7a3ef616e33c44d3e0ccbd28
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-05-28 13:59:15 +0530

    Project 1: Student performance indicator- exception handling code

---
+++ Projects/Student_Performance_Indicator/src/exception.py
@@ -0,0 +1,16 @@
+import sys
+
+def error_message_details(error, error_details:sys):
+
+    exc_type, exc_value, exc_tb = error_details.exc_info()
+    filename = exc_tb.tb_frame.f_code.co_filename
+    lineno = exc_tb.tb_lineno
+    error_message = f"Error occurred in file {filename} at line number {lineno} : {str(error)}"
+    return error_message
+    
+
+class CustomException(Exception):
+
+    def __init__(self, error_message):
+        super.__init__(error_message)
+        self.error_message = error_message_details(error, )
