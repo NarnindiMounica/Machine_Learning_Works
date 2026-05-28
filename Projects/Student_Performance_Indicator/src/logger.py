f7873f4c62dcf6c4b4bd96fb690936ac3d47982b
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-05-28 14:50:44 +0530

    Project 1: Student performance indicator- logging code

--- Projects/Student_Performance_Indicator/src/logger.py
+++ Projects/Student_Performance_Indicator/src/logger.py
@@ -2,13 +2,16 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.log"
-log_path = os.path.join(os.getcwd(), "log")
+LOG_FILE = f"{datetime.now().strftime("%Y-%m-%d %H-%M-%S")}.log"
+log_path = os.path.join(os.getcwd(), "logs")
 os.makedirs(log_path, exist_ok=True)
 
 logs_path = os.path.join(log_path, LOG_FILE)
 logging.basicConfig(
-    filename = logs_path,
-    level = logging.info,
-    format = "[%(asctime)s] - "
+    filename=logs_path,
+    level=logging.INFO,
+    format="[%(asctime)s] - %(lineno)d  %(name)s - %(levelname)s - %(message)s"
 )
+
+if __name__=="__main__":
+    logging.info("logging has started")
