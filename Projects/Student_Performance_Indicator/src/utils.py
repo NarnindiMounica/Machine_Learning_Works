28cbfd7d5ac2a87981e4392b0be6816bc54f1a94
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-05-30 23:15:29 +0530

    Project 1: Student performance indicator- save object function definition in utils

--- Projects/Student_Performance_Indicator/src/utils.py
+++ Projects/Student_Performance_Indicator/src/utils.py
@@ -1,2 +1,21 @@
 import os, sys
-imort dill
+import dill
+
+import numpy as np
+import pandas as pd
+
+from src.exception import CustomException
+
+
+def save_object(filepath, obj):
+
+    try:
+
+        file_dir = os.path.dirname(filepath)
+        os.makedirs(file_dir, exist_ok=True)
+
+        with open(filepath, 'wb') as file_obj:
+            dill.dump(obj, file_obj )
+
+    except Exception as e:
+        raise CustomException(e, sys)        
