11cb2a2d8721549a70c3ed32caf88d28c4a8ca5b
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-01 00:37:10 +0530

    Project 1: Student performance indicator - working project

--- Projects/Student_Performance_Indicator/app.py
+++ Projects/Student_Performance_Indicator/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("predictdata", methods=['GET', 'POST'])
+@app.route("/predict_data", methods=['GET', 'POST'])
 def predict_datapoint():
     if request.method=="GET":
         return render_template("home.html")
@@ -35,4 +35,4 @@ def predict_datapoint():
 
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)    
+    app.run(host="0.0.0.0", debug=True)    
