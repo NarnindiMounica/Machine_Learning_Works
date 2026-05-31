cf8e725a30b0aa7bb20d455b01b8c609acd6e694
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-01 00:28:56 +0530

    Project 1: Student performance indicator - prediction pipeline completed

--- Projects/Student_Performance_Indicator/src/pipeline/prediction_pipeline.py
+++ Projects/Student_Performance_Indicator/src/pipeline/prediction_pipeline.py
@@ -12,9 +12,19 @@ class PredictPipeline:
         pass
 
     def predict(self, features):
-         model_path="artifacts\\trained_model.pkl"
-         preprocessor_path="artifacts\\preprocessor.pkl"
-         model = load_object(filepath=model_path)
+        try:
+            model_path="artifacts\\trained_model.pkl"
+            preprocessor_path="artifacts\\preprocessor.pkl"
+            model = load_object(filepath=model_path)
+            preprocessor = load_object(filepath=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+
+            return preds
+        
+        except Exception as e:
+            raise CustomException(e, sys)
+         
 
 class CustomData:
     def __init__(self,
