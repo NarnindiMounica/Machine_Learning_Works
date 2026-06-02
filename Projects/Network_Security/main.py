9d31f5cdea49795820cdce12ac708562e177cbde
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-03 01:26:54 +0530

    Project 2: Network Security -custom exception and logging done

--- Projects/Network_Security/main.py
+++ Projects/Network_Security/main.py
@@ -2,10 +2,4 @@ import os, sys
 from networksecurity.exception_details.exception import CustomException
 from networksecurity.logging_details.logger import logging
 
-if __name__=="__main__":
-    logging.info("testing custom exception")
-    try:
-        a = 1/0
-        print("a will not be printed")
-    except Exception as e:
-        raise CustomException(e, sys)        
+      
