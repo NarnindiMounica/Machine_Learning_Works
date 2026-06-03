1eb4aab4b9fa44abb4710f3548e3ecff2c7a96c0
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-04 01:32:41 +0530

    Testing mongoclient connection

---
+++ Projects/Network_Security/push_data.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+
+uri = "mongodb+srv://mounicanarnindi12:moni1236@cluster0.uci075v.mongodb.net/?appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
