d2ddcba456946b2fe3c1f044a1b89e5e2d9678f6
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-04 23:34:30 +0530

    Project 2: Network Security - pushing_data into mongodb cluster

--- Projects/Network_Security/push_data.py
+++ Projects/Network_Security/push_data.py
@@ -2,9 +2,24 @@ import os, sys,json, certifi
 from dotenv import load_dotenv
 import pandas as pd
 import numpy as np
+import pymongo
 
+from networksecurity.exception_details.exception import CustomException
+from networksecurity.logging_details.logger import logging
 
 load_dotenv()
 ca=certifi.where()
 
 mongodb_url = os.getenv("MONGODB_URL")
+
+class NetworkDataExtract:
+    def __init__(self, filepath):
+        self.filepath = filepath
+
+    def csv_to_json_converter(self):
+        try:
+            pass
+        except Exception as e:
+            raise CustomException(e, sys)
+
+        
