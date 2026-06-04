ad92b3e337f1d10c082affa9664c31bb640ae9dd
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-04 23:09:58 +0530

    mongodb client connection testing

--- Projects/Network_Security/test_mongodb.py
+++ Projects/Network_Security/test_mongodb.py
@@ -7,5 +7,12 @@ load_dotenv()
 
 mongodb_url = os.getenv("MONGODB_URL")
 
+# Create a new client and connect to the server
+client = MongoClient(mongodb_url)
 
-print(mongodb_url)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
