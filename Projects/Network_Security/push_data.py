31b3700ccb3e1ff18823130008f54345d8f13190
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-05 00:46:05 +0530

    Project 2: Network Security - pushing_data into mongodb cluster

--- Projects/Network_Security/push_data.py
+++ Projects/Network_Security/push_data.py
@@ -13,8 +13,11 @@ ca=certifi.where()
 mongodb_url = os.getenv("MONGODB_URL")
 
 class NetworkDataExtract:
-    def __init__(self, filepath):
+    def __init__(self, filepath, records, collection, database):
         self.filepath = filepath
+        self.records = records
+        self.collection = collection
+        self.database = database
 
     def csv_to_json_converter(self):
         try:
@@ -25,6 +28,20 @@ class NetworkDataExtract:
         except Exception as e:
             raise CustomException(e, sys)
         
-    def insert_data_mongodb(self, )    
+    def insert_data_mongodb(self):
+        try:
+            self.mongo_client = pymongo.MongoClient(mongodb_url)
+            self.database = self.mongo_client[self.database]
+            self.collection = self.database[self.collection]
+            self.collection.insert_many(self.records)
+            return len(self.records)
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+
+if __name__=="__main__":
+            
+
 
         
