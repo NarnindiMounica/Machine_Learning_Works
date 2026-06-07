52831fe6d1512e9227a9889c262b8857fa9cc4a8
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-07 16:26:26 +0530

    Project 2: Network Security - functions to validate number of columns and numerical columns with schema

--- Projects/Network_Security/networksecurity/components/data_validation.py
+++ Projects/Network_Security/networksecurity/components/data_validation.py
@@ -23,4 +23,61 @@ class DataValidation:
             self.data_validation_config=data_validation_config
             self._schema_config=read_yaml_file(filepath=SCHEMA_FILEPATH)
         except Exception as e:
+            raise CustomException(e, sys)  
+        
+
+    @staticmethod
+    def read_data(filepath:str)->pd.DataFrame:
+        try:
+            return pd.DataFrame(filepath)
+        except Exception as e:
+            raise CustomException(e, sys)  
+
+    def validate_number_of_columns(self, dataframe:pd.DataFrame)->bool:
+        try:
+            number_of_columns = len(self._schema_config['columns'])
+            logging.info(f"Required number of columns : {number_of_columns}")
+            logging.info(f"DataFrame has columns : {len(dataframe.columns)}")
+            if len(dataframe.columns) == number_of_columns:
+                return True
+            else:
+                return False
+
+        except Exception as e:
             raise CustomException(e, sys)    
+        
+    def validate_number_of_num_columns(self, dataframe:pd.DataFrame)->bool:
+        try:
+            number_of_num_columns = len(self._schema_config['numerical columns'])
+            logging.info(f"Number of numerical columns: {number_of_num_columns}")
+            number_of_num_columns_in_dataframe = len([column for column in dataframe.columns if dataframe[column].dtype!="str"])
+
+            if number_of_num_columns==number_of_num_columns_in_dataframe:
+                return True
+            else:
+                return False
+        except Exception as e:
+            raise CustomException(e, sys)    
+
+
+    def initiate_data_valiation(self)->DataValidationArtifact:
+        try:
+            train_filepath = self.data_ingestion_artifacts.train_filepath
+            test_filepath = self.data_ingestion_artifacts.test_filepath
+
+            #read the data from train and test filepaths
+
+            train_dataframe = DataValidation.read_data(filepath=train_filepath)
+            test_dataframe = DataValidation.read_data(filepath=test_filepath)
+
+            train_status = self.validate_number_of_columns(dataframe=train_dataframe)
+            if not train_status:
+                error_message=f"Train dataframe does not contain all columns.\n"
+
+            test_status = self.validate_number_of_columns(dataframe=test_dataframe)
+            if not test_status:
+                error_message=f"Test dataframe does not contain all columns.\n"        
+
+        except Exception as e:
+            raise CustomException(e, sys)    
+      
--- Projects/Network_Security/networksecurity/utils/main_utils/utils.py
+++ Projects/Network_Security/networksecurity/utils/main_utils/utils.py
@@ -11,3 +11,4 @@ def read_yaml_file(filepath:str)->dict:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise CustomException(e,sys)
+    
