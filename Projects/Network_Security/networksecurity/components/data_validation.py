1298bebe2590d9040a24e47a28ff88f987f21e42
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-07 15:37:10 +0530

    Project 2: Network Security - read_yaml_file function in utils

--- Projects/Network_Security/networksecurity/components/data_validation.py
+++ Projects/Network_Security/networksecurity/components/data_validation.py
@@ -6,6 +6,21 @@ from scipy.stats import ks_2samp
 from networksecurity.exception_details.exception import CustomException
 from networksecurity.logging_details.logger import logging
 
+from networksecurity.utils.main_utils.utils import read_yaml_file
+
 
 from networksecurity.entity.config_entity import DataValidationConfig
 from networksecurity.entity.artifact_entity import DataValidationArtifact, DataIngestionArtifact
+
+from networksecurity.constant.training_pipeline import SCHEMA_FILEPATH
+
+class DataValidation:
+    def __init__(self, data_ingestion_artifacts:DataIngestionArtifact,
+                 data_validation_config:DataValidationConfig):
+        
+        try:
+            self.data_ingestion_artifacts=data_ingestion_artifacts
+            self.data_validation_config=data_validation_config
+            self._schema_config=read_yaml_file(filepath=SCHEMA_FILEPATH)
+        except Exception as e:
+            raise CustomException(e, sys)    
---
+++ Projects/Network_Security/networksecurity/utils/main_utils/utils.py
@@ -0,0 +1,13 @@
+import os, sys, yaml, pickle, dill
+import numpy as np
+import pandas as pd
+
+from networksecurity.exception_details.exception import CustomException
+from networksecurity.logging_details.logger import logging
+
+def read_yaml_file(filepath:str)->dict:
+    try:
+        with open(filepath, 'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise CustomException(e,sys)
