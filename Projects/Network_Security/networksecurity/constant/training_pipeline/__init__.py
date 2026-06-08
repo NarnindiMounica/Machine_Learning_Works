ac3dc5949257efe2bbdcab135acef9c55a41e90c
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-08 23:29:11 +0530

    Project 2: Network Security - Model trainer defining constants

--- Projects/Network_Security/networksecurity/constant/training_pipeline/__init__.py
+++ Projects/Network_Security/networksecurity/constant/training_pipeline/__init__.py
@@ -43,5 +43,13 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "weights": "uniform"
 }
 
+''' Model trainer related constants starts with MODEL_TRAINER var name'''
+
+MODEL_TRAINER_DIR_NAME:str = "model_trainer"
+MODEL_TRAINER_TRAINED_MODEL_DIR:str = "trained_model"
+MODEL_TRAINER_TRAINED_MODEL_NAME:str = "model.pkl"
+MODEL_TRAINER_EXPECTED_SCORE:float = 0.6
+MODEL_TRAINER_OVER_FITTING_UNDER_FITTING_THRESHOLD:float = 0.05
+
 
 
