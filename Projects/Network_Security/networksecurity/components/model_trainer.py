8ee4122c866219c8ab5e1413df4517aa677bd17e
NarnindiMounica <mounica.narnindi12@gmail.com>
2026-06-10 14:39:21 +0530

    Project 2: Network Security - train_model function definition

--- Projects/Network_Security/networksecurity/components/model_trainer.py
+++ Projects/Network_Security/networksecurity/components/model_trainer.py
@@ -10,15 +10,56 @@ from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.utils.main_utils.utils import save_object, save_numpy_array_data, load_numpy_array_data
 from networksecurity.utils.ml_utils.metric.classification_metric import get_classification_score
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
 
 class ModelTrainer:
     def __init__(self, model_trainer_config:ModelTrainerConfig, data_transformation_artifact:DataTransformationArtifact):
         self.model_trainer_config = model_trainer_config
         self.data_transformation_artifact = data_transformation_artifact
 
-    def train_model(self, x_train, y_train):
+    def train_model(self, x_train, y_train, x_test, y_test):
         try:
+            models = {
+                "Random_Forest":RandomForestClassifier(verbose=True),
+                "KNN": KNeighborsClassifier(),
+                "Decision_Tree":DecisionTreeClassifier(),
+                "Logistic_Regression": LogisticRegression(verbose=True),
+                "AdaBoost":AdaBoostClassifier(),
+                "GradientBoost":GradientBoostingClassifier(verbose=True)
+                }
             
+            params = {
+                "Random_Forest":{
+                    "n_estimators":[8,16,32,64,128,156],
+                    "criterion":['gini', 'entropy'],
+                    "max_features":['sqrt', 'log2', None]
+                },
+
+                "Decision_Tree":{
+                    "criterion":['gini', 'entropy'],
+                    "splitter":['best', 'random'],
+                    "max_features":['sqrt', 'log2']
+                },
+                "Logistic_Regression":{},
+                "KNN":{"n_neighbors":[5,6,7,8]},
+                "AdaBoost":{
+                    "learning_rate":[.1, .01, .05, .001],
+                    "n_estimators": [8,16,32,64,128,156],
+                },
+                "GradientBoost":{
+                    "loss":['log_loss', 'exponential'],
+                    "learning_rate":[.1, .01, .05, .001],
+                    "subsample":[0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    "n_estimators": [8,16,32,64,128,156],
+                    "criterion":['squared_error', 'friedman_mse'],
+                    "max_features":['auto', 'sqrt', 'log2']
+                }
+            }
+
+            model_report
         except Exception as e:
             raise CustomException(e, sys)    
 
